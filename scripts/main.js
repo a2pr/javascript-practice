6c25c7465e07cab9dc67d16baa23490fcbb716f0
a2pr <a2payema@gmail.com>
2017-04-10 09:00:11 -0400

    Aventura javascript MK2

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,51 @@
-var a=prompt("Your name");
- if(a!="") {
-     alert("hi Mr or Ms"+ " " +a);
-     var b=prompt("Como quiere iniciar su aventura? como Mago=1, como Warrior=2, como Archer=3");
+function randomNumber(range){
+    return Math.round( Math.random() * range );
+} 
+var  eventList=[
+    "You woke up in a cementary, with nothing in your pockets",
+    "You open your eyes and feel the freeze cold wind of the glaciar",
+    "You appear in the middle of a forest, with trees as high as the eye can see"
+];
+var classList=[
+    "Mage",
+    "Warrior",
+    "Archer"
+];
+var mageSpellList=[
+    "fireball",
+    "ice Age",
+    "earth manipulation"
+];
+var weaponList=[
+    "Sword",
+    "spear",
+    "Axe",
+    "Dagger",
+    "Bow & arrows",
+    "slinger"
+];
+var warriorSkillList=[
+
+];
+
+alert(eventList[randomNumber( eventList.length - 1)]);
+var name= prompt("A crow lands on your shoulder, and start to speaks'Whats your name stranger?'");
+ if(name!="") {
+     alert("hi Mr or Ms"+ " " + name + " " + " You are on top of my home. So i demand you, to look for a new home for me and my children " );
+     var b=prompt("Do you have any special skills? are you a Mage?, or Warrior?, or perphaps an Archer? (Mage=1, Warrior=2, Archer=3)");
     }
+    if (b==1 && b!=" ") {
+        var c= prompt("Oh how lucky i'm. Can you fix my house " + " "+ classList[b -1]+"? non then what can youdo?"+" which arcane magic do you posses? perphaps"
+         +"you can use destruction with fireballs? Or maybe you control the tempature with Ice age? or you could shake the ground itself"+ 
+         "with Earth manipulation? (fireball=1, Ice age=2, Eart manipulation=3) ");
+        alert("lets go to that cabin over there it could be a great place for my children");
+
+    }else if( b>1){
+        alert("Great im stuck with a lowly"+" "+ classList[b-1] +" "+" go grab some weapon in that cabin over there");
+    }
+
+
+/*
 if(b==1 && b!=" "){
     var c=prompt("Cual spell quieres aprender? Para fireball=1, Ice age=2, Earth manipulation=3");
     var d=prompt("Esta seguro? yes=1, no=2");
@@ -45,4 +88,4 @@ else if(b==3 && b!=" "){
         else if(d==1 && c==3){
             alert("Suerte en tu aventura"+" "+ "Aprendiz de Archer" +" " +a+" "+"master of the Silver bow");
         }
-}
+}*/
