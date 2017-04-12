6972b84fc7821a49b89a2691bfa57db8bd442001
a2pr <a2payema@gmail.com>
2017-04-12 08:53:39 -0400

    MK3.part2

--- scripts/main.js
+++ scripts/main.js
@@ -12,6 +12,7 @@ var mageSpellList=[
     "ice Age",
     "earth manipulation"
 ];
+*/
 var weaponList=[
     "Sword",
     "spear",
@@ -20,6 +21,10 @@ var weaponList=[
     "Bow & arrows",
     "slinger"
 ];
+function randomWeapon (range) {
+    return Math.round(Math.random() * range);
+};
+/*
 var warriorSkillList=[
     "Vertical slash",
     "Flury",
@@ -46,29 +51,55 @@ alert("hi Mr or Ms"+ " " + name + " " + " You are on top of my home. So i demand
 hero.characterClass= window.prompt("Do you have any special skills? are you a Mage?, or Warrior?, or perphaps an Archer?").toLowerCase();
 
 if ( hero.characterClass=== "mage") {
+        alert("You choose mage class. Base Stats are as follows: Damage= 1, Health= 4, Agility=4");
+        hero.damage=1;
+        hero.health=4;
+        hero.agility=4;
         hero.skill =  prompt("Oh how lucky i'm. Can you fix my house " + " "+ hero.characterClass +"? no then what can you do?"+" which arcane magic do you posses? perphaps"
          +"you can cause destruction with fireballs? Or maybe you control the tempature with Ice age? or you could shake the ground itself"+ 
          "with Earth manipulation?").toLowerCase();
+         if (hero.skill=== "fireballs") {
+             alert("You choose fireball, base stats updated: damage +6; health +2");
+             hero.damage=7;
+             hero.health=6;
+         }
+         else if(hero.skill=== "ice age"){
+             alert("You choose Ice age, base stats updated: damage +6; agility +2");
+             hero.damage=7;
+             hero.agility=6;
+         }
+         else if (hero.skill=== "earth manipulation") {
+             alert("You choose Earth manipulation, base stats updated: damage +4; agility +1; health +1");
+             hero.damage=5;
+             hero.agility=5;
+             hero.agility=5;
+         }
+         
+}else if (hero.characterClass === "warrior" || hero.characterClass === "archer" ) {
+    alert("Great im stuck with a lowly" + " "+ hero.characterClass).toLowerCase();
+    if (hero.characterClass ==="warrior") {
+        alert("You choose warrior class. Base stats are as follows: Damage=4, Health= 4, Agility=2");
+        hero.damage=4;
+        hero.health=4;
+        hero.agility=1;
+        aler("Warriors can use ");//last part 
+    }
 }
-alert(hero.skill);          
-        /*alert("lets go to that cabin over there it could be a great place for my children");
+alert("lets go to that cabin over there it could be a great place for my children");
+alert("You follow the crow request and go to the cabin.");
 
-    }else if( b>1){
-        alert("Great im stuck with a lowly"+" "+ classList[b-1] +" "+" go grab some weapon in that cabin over there");
+if (hero.characterClass ==="mage") {
+    alert("Inside the cabin you found a closet fill with:");
+    alert("nothing.....");
+    alert("you got quite the luck my mage friend-said the crow");
+}else if(hero.characterClass === "warrior" || hero.characterClass === "archer"){
+    alert("Inside the cabin you found a:");
+    hero.weapon=prompt(weaponList[randomWeapon(weaponList.length-1)]+" "+"Do you pick it?").toLowerCase();
+    if (hero.characterClass ==="warrior" && hero.weapon ==="sword" || hero.weapon ==="Axe" || hero.weapon ==="sword" ) {
+        alert("plus 5 damage");   
     }
-
-alert("You follow the crow advice and go to the cabin.");
-if (b>1) {
-    prompt("Inside the cabin you found a closet fill with:"+ weaponList[randomNumber(weaponList.lenght-1)]+" "+
-    weaponList[randomNumber(weaponList.lenght-1)]+" "+ weaponList[randomNumber(weaponList.lenght-1)]+" "+
-    "Which one do you pick?");
-}else{
-    var d= prompt("You enter the inside of the dark cabin, a creature baring his fangs at you, What you do? run=1, attack=2, grab the crow and throw it to the creature=3");
-    if (d<) {
-        
-    }
-}*/
-
+}
+ prompt("A creature appears at the entraces of the cabin, baring his fangs at you, What you do? run, attack, grab the crow and throw it to the creature");
 /*
 if(b==1 && b!=" "){
     var c=prompt("Cual spell quieres aprender? Para fireball=1, Ice age=2, Earth manipulation=3");
